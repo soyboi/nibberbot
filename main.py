64bc5cc3234b7dd83a33f44bd498c272dfbd0344
Croix <correct_email@correct_host.com>
2022-10-25 22:53:36 +0300

    add functionality to list user-addable roles

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ async def on_ready():
     print(f'Logged in as {client.user} ID: {client.user.id}')
     print('--------')
 
+
 @client.tree.command()
 async def hello(interaction: discord.Interaction):
 
     await interaction.response.send_message(f'wassup ni🅱🅱a, {interaction.user.mention}')
 
-@client.tree.command()
-async def gibrole(interaction: discord.Interaction, arg: str):
+
+@client.tree.command(name='gib_role')
+async def gib_role(interaction: discord.Interaction, arg: str):
 
     roles = []
     addable_roles = []
-
     role_to_add = None
-
     flag = False
 
     for role in interaction.user.guild.roles:
@@ -47,8 +47,9 @@ async def gibrole(interaction: discord.Interaction, arg: str):
     else:
         await interaction.response.send_message('no such role niba')
 
-@client.tree.command()
-async def ungibrole(interaction: discord.Interaction, arg: str):
+
+@client.tree.command(name='ungib_role')
+async def ungib_role(interaction: discord.Interaction, arg: str):
 
     roles = []
     addable_roles = []
@@ -75,6 +76,23 @@ async def ungibrole(interaction: discord.Interaction, arg: str):
         await interaction.response.send_message(f'role {role_to_remove.name} ungiben to {interaction.user.mention}')
     else:
         await interaction.response.send_message('no such role niba')
+
+@client.tree.command(name='list_addable_roles')
+async def list_roles(interaction: discord.Interaction):
+
+    addable_roles = []
+
+    flag = False
+
+    for role in interaction.user.guild.roles:
+
+        if flag is not True and role.name == 'limiter':
+            flag = True
+
+        if flag is False and role.name != '@everyone':
+            addable_roles.append(role.name)
+
+    await interaction.response.send_message(addable_roles)
 
 
 client.run(config['AUTH']['token'])
--- nibberbot.py
+++ nibberbot.py
@@ -1,15 +1,14 @@
 import discord
 from discord import app_commands
 
+
 class Nibberbot(discord.Client):
 
-    def __init__(self, *, intents:discord.Intents):
-
+    def __init__(self, *, intents: discord.Intents):
         super().__init__(intents=intents)
 
         self.tree = app_commands.CommandTree(self)
 
     async def setup_hook(self):
-
         await self.tree.fetch_commands()
         await self.tree.sync()
